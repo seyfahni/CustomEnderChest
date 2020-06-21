ed4adf9917baec0bb10074ab8694efaa054b05a9
Niklas Seyfarth <niklas@seyfarth.de>
2020-06-21 14:52:03 +0200

    Cleanup EnderChestUtil

--- src/main/java/net/craftersland/customenderchest/utils/EnderChestUtils.java
+++ src/main/java/net/craftersland/customenderchest/utils/EnderChestUtils.java
@@ -1,7 +1,5 @@
 package net.craftersland.customenderchest.utils;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 import net.craftersland.customenderchest.EnderChest;
@@ -11,57 +9,58 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.permissions.Permissible;
 
 public class EnderChestUtils {
 	
-	private EnderChest enderchest;
+	private final EnderChest enderchest;
 	
 	public EnderChestUtils(EnderChest enderchest) {
 		this.enderchest = enderchest;
 	}
 	
     public String getTitle(Player p) {
-		
-		List<String> chestTitle = new ArrayList<String>();
-		chestTitle.add(enderchest.getConfigHandler().getString("enderChestTitle.enderChestName"));
-		
-		if (p.hasPermission("CustomEnderChest.level.5")) {
-			chestTitle.set(0, chestTitle.get(0).replaceAll("%level", enderchest.getConfigHandler().getString("enderChestTitle.level5")));
-		} else if (p.hasPermission("CustomEnderChest.level.4") && !p.isOp()) {
-			chestTitle.set(0, chestTitle.get(0).replaceAll("%level", enderchest.getConfigHandler().getString("enderChestTitle.level4")));
-		} else if (p.hasPermission("CustomEnderChest.level.3") && !p.isOp()) {
-			chestTitle.set(0, chestTitle.get(0).replaceAll("%level", enderchest.getConfigHandler().getString("enderChestTitle.level3")));
-		} else if (p.hasPermission("CustomEnderChest.level.2") && !p.isOp()) {
-			chestTitle.set(0, chestTitle.get(0).replaceAll("%level", enderchest.getConfigHandler().getString("enderChestTitle.level2")));
-		} else if (p.hasPermission("CustomEnderChest.level.1") && !p.isOp()) {
-			chestTitle.set(0, chestTitle.get(0).replaceAll("%level", enderchest.getConfigHandler().getString("enderChestTitle.level1")));
-		} else if (p.hasPermission("CustomEnderChest.level.0") && !p.isOp()) {
-			chestTitle.set(0, chestTitle.get(0).replaceAll("%level", enderchest.getConfigHandler().getString("enderChestTitle.level0")));
-		}
-		
-		chestTitle.set(0, chestTitle.get(0).replaceAll("%player", p.getName()));
-		
-		if (chestTitle.get(0).length() <= 32) {
-			String enderChestTitle = chestTitle.get(0).replaceAll("&", "§");
-			return enderChestTitle;
-		} else {
-			String enderChestTitle = chestTitle.get(0).substring(0, 32).replaceAll("&", "§");
-			return enderChestTitle;
-		}
-	}
-    
-    public String getCmdTitle(Player p) {
-		String chestTitle = ChatColor.DARK_PURPLE + "" + ChatColor.BOLD + "" + p.getName() + "'s " + ChatColor.LIGHT_PURPLE + "Ender Chest";
 
-		if (chestTitle.length() <= 32) {
-			return chestTitle.replaceAll("&", "§");
-		} else {
-			return chestTitle.substring(0, 32).replaceAll("&", "§");
+		int enderchestLevel = getEnderchestLevel(p);
+
+		String chestTitle = enderchest.getConfigHandler().getString("enderChestTitle.enderChestName")
+				.replace("${level}", enderchest.getConfigHandler().getString("enderChestTitle.level." + enderchestLevel))
+				.replace("${player}", p.getName())
+				.replace("${$}", "$");
+
+		if (chestTitle.length() > 32) {
+			chestTitle = chestTitle.substring(0, 32);
 		}
+
+		return chestTitle.replace('&', '§');
+	}
+
+	private int getEnderchestLevel(Permissible p) {
+		return getEnderchestLevel(p, 5);
+	}
+
+	private int getEnderchestLevel(Permissible p, int maxLevel) {
+		if (p.isOp()) {
+			return maxLevel;
+		}
+		for (int level = maxLevel; level >= 0; --level) {
+			if (p.hasPermission("CustomEnderChest.level." + level)) {
+				return level;
+			}
+		}
+		return -1;
+	}
+
+	public String getCmdTitle(Player p) {
+		return getCmdTitle(p.getName());
 	}
     
     public String getCmdTitle(UUID p) {
-		String chestTitle = ChatColor.DARK_PURPLE + "" + ChatColor.BOLD + "" + enderchest.getStorageInterface().loadName(p) + "'s " + ChatColor.LIGHT_PURPLE + "Ender Chest";
+		return getCmdTitle(enderchest.getStorageInterface().loadName(p));
+	}
+
+	private String getCmdTitle(String name) {
+		String chestTitle = ChatColor.DARK_PURPLE + "" + ChatColor.BOLD + "" + name + "'s " + ChatColor.LIGHT_PURPLE + "Ender Chest";
 
 		if (chestTitle.length() <= 32) {
 			return chestTitle.replaceAll("&", "§");
@@ -69,29 +68,10 @@ public class EnderChestUtils {
 			return chestTitle.substring(0, 32).replaceAll("&", "§");
 		}
 	}
-    
-    public Integer getSize(Player p) {
+
+	public Integer getSize(Permissible p) {
     	
-    	if (p.hasPermission("CustomEnderChest.level.5")) {
-			return 54;
-		}
-    	if (p.hasPermission("CustomEnderChest.level.4") && !p.isOp()) {
-			return 45;
-		}
-    	if (p.hasPermission("CustomEnderChest.level.3") && !p.isOp()) {
-			return 36;
-		}
-    	if (p.hasPermission("CustomEnderChest.level.2") && !p.isOp()) {
-			return 27;
-		}
-    	if (p.hasPermission("CustomEnderChest.level.1") && !p.isOp()) {
-			return 18;
-		}
-    	if (p.hasPermission("CustomEnderChest.level.0") && !p.isOp()) {
-			return 9;
-		}
-		
-		return 0;
+    	return (getEnderchestLevel(p) + 1) * 9;
 	}
     
     public void openMenu(Player p) {
@@ -114,17 +94,16 @@ public class EnderChestUtils {
 			Inventory newInv = Bukkit.getServer().createInventory(p, size, enderChestTitle);
 			if (size > inv.getSize()) {
 				//TODO run this async to prevent tps drops on slow connections
-				if (enderchest.getStorageInterface().hasDataFile(p.getUniqueId()) == true) {
+				if (enderchest.getStorageInterface().hasDataFile(p.getUniqueId())) {
 					enderchest.getStorageInterface().loadEnderChest(p, newInv);
 				}
-				inv = newInv;
 			} else {
 				for (int i = 0; i < size; i++) {
 	    			ItemStack item = inv.getItem(i);
 	    			newInv.setItem(i, item);
 	            }
-				inv = newInv;
 			}
+			inv = newInv;
 		}
 		enderchest.getDataHandler().setData(p.getUniqueId(), inv);
 		enderchest.getSoundHandler().sendEnderchestOpenSound(p);
